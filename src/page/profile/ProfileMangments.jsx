04a07d2cf667f2731b651bd5aee5faf91ca77074
mohamed2678 <mohamedgalal2729@gmail.com>
2025-08-31 14:37:32 +0300

    finish from form and make validate inputs

--- src/page/profile/ProfileMangments.jsx
+++ src/page/profile/ProfileMangments.jsx
@@ -1,19 +1,64 @@
 import { Box, Button, MenuItem, Stack, TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import { useForm } from "react-hook-form";
+
+const regEmail =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const regPhone =
+  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+
+const currencies = [
+  {
+    value: "user",
+    label: "User",
+  },
+  { value: "admin", label: "Admin" },
+  { value: "Manger", label: "Manger" },
+];
 
 function ProfileMangments() {
-  const currencies = [
-    {
-      value: "user",
-      label: "User",
-    },
-    { value: "admin", label: "Admin" },
-    { value: "Manger", label: "Manger" },
-  ];
+
+  const [state, setState] = useState({
+    open: false,
+    vertical: "top",
+    horizontal: "right",
+  });
+
+
+  const reset = () => {
+    document.querySelector("form").reset();
+  };
+
+  const { vertical, horizontal, open } = state;
+
+  const handleClick = (newState) => () => {
+    setState({ ...newState, open: true });
+  };
+
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
+    console.log("submitted");
+    handleClick({ vertical: "top", horizontal: "right" })();
+    reset();
+  };
 
   return (
     <Box
       component="form"
+      onSubmit={handleSubmit(onSubmit)}
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -23,12 +68,45 @@ function ProfileMangments() {
       autoComplete="off"
     >
       <Stack direction="row" spacing={2}>
-        <TextField sx={{ flex: 1 }} label="first name" variant="filled" />
-        <TextField sx={{ flex: 1 }} label="Last name" variant="filled" />
+        <TextField
+          error={errors.firstName}
+          helperText={
+            errors.firstName && "First name is required & min 3 charachters"
+          }
+          {...register("firstName", { required: true, minLength: 3 })}
+          sx={{ flex: 1 }}
+          label="first name"
+          variant="filled"
+        />
+
+        <TextField
+          error={errors.lastname}
+          helperText={
+            errors.lastname && "Last name is required & min 3 charachters"
+          }
+          {...register("lastname", { required: true, minLength: 3 })}
+          sx={{ flex: 1 }}
+          label="Last name"
+          variant="filled"
+        />
       </Stack>
 
-      <TextField id="filled-basic" label="email" variant="filled" />
-      <TextField id="filled-basic" label="Contact Number" variant="filled" />
+      <TextField
+        error={errors.email}
+        helperText={errors.email && "Please enter a valid email address"}
+        {...register("email", { required: true, pattern: regEmail })}
+        id="filled-basic"
+        label="email"
+        variant="filled"
+      />
+      <TextField
+        error={errors.phone}
+        helperText={errors.phone && "please enter a valid phone number"}
+        {...register("phone", { required: true, pattern: regPhone })}
+        id="filled-basic"
+        label="Contact Number"
+        variant="filled"
+      />
       <TextField id="filled-basic" label="Address 1" variant="filled" />
       <TextField id="filled-basic" label="Address 2" variant="filled" />
       <TextField
@@ -43,9 +121,23 @@ function ProfileMangments() {
           </MenuItem>
         ))}
       </TextField>
-        <Box sx={{textAlign: "right"}}>
-          <Button variant="contained">Create New User</Button>
-        </Box>
+      <Box sx={{ textAlign: "right" }}>
+        <Button
+          type="submit"
+          variant="contained"
+        >
+          Create New User
+        </Button>
+        <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          onClose={handleClose}
+          message="User Created Successfully"
+          key={vertical + horizontal}
+          autoHideDuration={3000}
+          sx={{ mt: 3 }}
+        />
+      </Box>
     </Box>
   );
 }
