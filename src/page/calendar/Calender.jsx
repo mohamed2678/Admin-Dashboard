78e6d55fc66369280b5f6b9981f23b51b077a22e
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-01 15:24:23 +0300

    fix errors on dark mode and finish from calender page and FAQs......

--- src/page/calendar/Calender.jsx
+++ src/page/calendar/Calender.jsx
@@ -90,27 +90,9 @@ function renderEventContent(eventInfo) {
   )
 }
 
-function Sidebar({ weekendsVisible, handleWeekendsToggle, currentEvents }) {
+function Sidebar({ currentEvents }) {
   return (
     <div className='demo-app-sidebar'>
-      <div className='demo-app-sidebar-section'>
-        <h2>Instructions</h2>
-        <ul>
-          <li>Select dates and you will be prompted to create a new event</li>
-          <li>Drag, drop, and resize events</li>
-          <li>Click an event to delete it</li>
-        </ul>
-      </div>
-      <div className='demo-app-sidebar-section'>
-        <label>
-          <input
-            type='checkbox'
-            checked={weekendsVisible}
-            onChange={handleWeekendsToggle}
-          ></input>
-          toggle weekends
-        </label>
-      </div>
       <div className='demo-app-sidebar-section'>
         <h2>All Events ({currentEvents.length})</h2>
         <ul>
