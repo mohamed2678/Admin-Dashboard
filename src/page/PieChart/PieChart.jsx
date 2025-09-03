90c06eb3a1a018d151dd5007239c0e3f9c24d7f7
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-03 14:39:11 +0300

    finish pie chart

--- src/page/PieChart/PieChart.jsx
+++ src/page/PieChart/PieChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ResponsivePie } from '@nivo/pie'
+import { Box, useTheme } from '@mui/material'
 
 
 const data = [
@@ -36,17 +37,129 @@ const data = [
 ]
 
 function PieChart() {
+  const theme = useTheme();
+
+
   return (
-    <div>
+    <Box sx={{height: "75vh"}}>
        <ResponsivePie /* or Pie for fixed dimensions */
         data={data}
+            theme={{
+          text: {
+            fontSize: 11,
+            fill: theme.palette.text.primary,
+            outlineWidth: 0,
+            outlineColor: "#ffffff",
+          },
+          axis: {
+            domain: {
+              line: {
+                stroke: theme.palette.text.primary,
+                strokeWidth: 1,
+              },
+            },
+            legend: {
+              text: {
+                fontSize: 12,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "#ffffff",
+              },
+            },
+            ticks: {
+              line: {
+                stroke: theme.palette.text.primary,
+                strokeWidth: 1,
+              },
+              text: {
+                fontSize: 11,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "#ffffff",
+              },
+            },
+          },
+          grid: {
+            line: {
+              stroke: "#dddddd",
+              strokeWidth: 1,
+            },
+          },
+          legends: {
+            title: {
+              text: {
+                fontSize: 11,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "#ffffff",
+              },
+            },
+            text: {
+              fontSize: 11,
+              fill: theme.palette.text.primary,
+              outlineWidth: 0,
+              outlineColor: "#ffffff",
+            },
+            ticks: {
+              line: {},
+              text: {
+                fontSize: 10,
+                fill: theme.palette.text.primary,
+                outlineWidth: 0,
+                outlineColor: "#ffffff",
+              },
+            },
+          },
+          annotations: {
+            text: {
+              fontSize: 13,
+              fill: theme.palette.text.primary,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+            link: {
+              stroke: theme.palette.text.primary,
+              strokeWidth: 1,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+            outline: {
+              stroke: theme.palette.text.primary,
+              strokeWidth: 2,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+            symbol: {
+              fill: theme.palette.text.primary,
+              outlineWidth: 2,
+              outlineColor: "#ffffff",
+              outlineOpacity: 1,
+            },
+          },
+          tooltip: {
+            wrapper: {},
+            container: {
+              background: theme.palette.background.paper,
+              color: theme.palette.text.primary,
+              fontSize: 12,
+            },
+            basic: {},
+            chip: {},
+            table: {},
+            tableCell: {},
+            tableCellValue: {},
+          },
+        }}
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
         innerRadius={0.5}
         padAngle={0.6}
         cornerRadius={2}
         activeOuterRadiusOffset={8}
         arcLinkLabelsSkipAngle={10}
-        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsTextColor= {theme.palette.text.primary}
         arcLinkLabelsThickness={2}
         arcLinkLabelsColor={{ from: 'color' }}
         arcLabelsSkipAngle={10}
@@ -62,7 +175,7 @@ function PieChart() {
             }
         ]}
     />
-    </div>
+    </Box>
   )
 }
 
