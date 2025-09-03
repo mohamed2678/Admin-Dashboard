85ab6091fadfaae6e46a19b918a1748caab5eeae
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-03 14:58:58 +0300

    create line chart and finish...

---
+++ src/page/lineChart/LineData.js
@@ -0,0 +1,268 @@
+const data = [
+  {
+    "id": "japan",
+    "data": [
+      {
+        "x": "plane",
+        "y": 65
+      },
+      {
+        "x": "helicopter",
+        "y": 97
+      },
+      {
+        "x": "boat",
+        "y": 144
+      },
+      {
+        "x": "train",
+        "y": 131
+      },
+      {
+        "x": "subway",
+        "y": 157
+      },
+      {
+        "x": "bus",
+        "y": 148
+      },
+      {
+        "x": "car",
+        "y": 223
+      },
+      {
+        "x": "moto",
+        "y": 12
+      },
+      {
+        "x": "bicycle",
+        "y": 190
+      },
+      {
+        "x": "horse",
+        "y": 243
+      },
+      {
+        "x": "skateboard",
+        "y": 36
+      },
+      {
+        "x": "others",
+        "y": 147
+      }
+    ]
+  },
+  {
+    "id": "france",
+    "data": [
+      {
+        "x": "plane",
+        "y": 74
+      },
+      {
+        "x": "helicopter",
+        "y": 15
+      },
+      {
+        "x": "boat",
+        "y": 186
+      },
+      {
+        "x": "train",
+        "y": 240
+      },
+      {
+        "x": "subway",
+        "y": 148
+      },
+      {
+        "x": "bus",
+        "y": 150
+      },
+      {
+        "x": "car",
+        "y": 227
+      },
+      {
+        "x": "moto",
+        "y": 184
+      },
+      {
+        "x": "bicycle",
+        "y": 66
+      },
+      {
+        "x": "horse",
+        "y": 236
+      },
+      {
+        "x": "skateboard",
+        "y": 36
+      },
+      {
+        "x": "others",
+        "y": 177
+      }
+    ]
+  },
+  {
+    "id": "us",
+    "data": [
+      {
+        "x": "plane",
+        "y": 119
+      },
+      {
+        "x": "helicopter",
+        "y": 211
+      },
+      {
+        "x": "boat",
+        "y": 124
+      },
+      {
+        "x": "train",
+        "y": 123
+      },
+      {
+        "x": "subway",
+        "y": 274
+      },
+      {
+        "x": "bus",
+        "y": 227
+      },
+      {
+        "x": "car",
+        "y": 90
+      },
+      {
+        "x": "moto",
+        "y": 199
+      },
+      {
+        "x": "bicycle",
+        "y": 41
+      },
+      {
+        "x": "horse",
+        "y": 45
+      },
+      {
+        "x": "skateboard",
+        "y": 241
+      },
+      {
+        "x": "others",
+        "y": 87
+      }
+    ]
+  },
+  {
+    "id": "germany",
+    "data": [
+      {
+        "x": "plane",
+        "y": 156
+      },
+      {
+        "x": "helicopter",
+        "y": 174
+      },
+      {
+        "x": "boat",
+        "y": 122
+      },
+      {
+        "x": "train",
+        "y": 130
+      },
+      {
+        "x": "subway",
+        "y": 235
+      },
+      {
+        "x": "bus",
+        "y": 78
+      },
+      {
+        "x": "car",
+        "y": 38
+      },
+      {
+        "x": "moto",
+        "y": 255
+      },
+      {
+        "x": "bicycle",
+        "y": 293
+      },
+      {
+        "x": "horse",
+        "y": 33
+      },
+      {
+        "x": "skateboard",
+        "y": 53
+      },
+      {
+        "x": "others",
+        "y": 178
+      }
+    ]
+  },
+  {
+    "id": "norway",
+    "data": [
+      {
+        "x": "plane",
+        "y": 269
+      },
+      {
+        "x": "helicopter",
+        "y": 128
+      },
+      {
+        "x": "boat",
+        "y": 112
+      },
+      {
+        "x": "train",
+        "y": 120
+      },
+      {
+        "x": "subway",
+        "y": 266
+      },
+      {
+        "x": "bus",
+        "y": 70
+      },
+      {
+        "x": "car",
+        "y": 48
+      },
+      {
+        "x": "moto",
+        "y": 240
+      },
+      {
+        "x": "bicycle",
+        "y": 76
+      },
+      {
+        "x": "horse",
+        "y": 150
+      },
+      {
+        "x": "skateboard",
+        "y": 150
+      },
+      {
+        "x": "others",
+        "y": 171
+      }
+    ]
+  }
+]
+export {data};
