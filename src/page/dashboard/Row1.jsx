7661fdc9cd93ee19deb77aad2da3930579e804ab
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-06 22:51:19 +0300

    take first step on dashboard page

---
+++ src/page/dashboard/Row1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Row1() {
+  return (
+    <div>Row1</div>
+  )
+}
+
+export default Row1
--- src/page/dashboard/dashboard.jsx
+++ src/page/dashboard/dashboard.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import Row1 from './Row1'
 import Row2 from './Row2'
 import Row3 from './Row3'
+import { Button } from '@mui/material'
+import { DownloadOutlined } from '@mui/icons-material'
+
 
 function dashboard() {
   return (
     <div>
+      <Button variant="text" color="primary">
+        <DownloadOutlined />
+        Download Reports
+      </Button>
       <Row1 />
       <Row2 />
       <Row3 />
