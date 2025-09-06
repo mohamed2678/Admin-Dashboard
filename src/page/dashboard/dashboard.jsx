582ba6cde77be96d1613638f9d91499fe1e22d23
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-06 22:46:15 +0300

    working on dashboard page

--- src/page/dashboard/dashboard.jsx
+++ src/page/dashboard/dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Row1 from './Row1'
+import Row2 from './Row2'
+import Row3 from './Row3'
 
 function dashboard() {
   return (
-    <div>dashboard</div>
+    <div>
+      <Row1 />
+      <Row2 />
+      <Row3 />
+    </div>
   )
 }
 
