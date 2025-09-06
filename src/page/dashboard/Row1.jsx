8e7805b646fd5da0611371d4d4cd6f893b981e57
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-07 00:32:09 +0300

    dashboard Page splitting to 3 rows and finish from  row1

--- src/page/dashboard/Row1.jsx
+++ src/page/dashboard/Row1.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import { data1, data2, data3, data4 } from "./Data";
+import { useTheme } from "@mui/material/styles";
+import Card from "./Card";
 
 function Row1() {
+    const theme = useTheme();
   return (
-    <div>Row1</div>
-  )
+    <Stack
+      direction={"row"}
+      flexWrap={"wrap"}
+      gap={1}
+      justifyContent={{ xs: "center", md: "space-between" }}
+    >
+      <Card
+        title={"12,361"}
+        icon={<EmailIcon sx={{ fontSize: "23px", color: theme.palette.secondary.main }} />}
+        subTitle={"Emails Sent"}
+        data={data1}
+        scheme={"nivo"}
+        percentage={"+14%"}
+      />
+      <Card
+        title={"431,225"}
+        icon={<PointOfSaleIcon sx={{ fontSize: "23px", color: theme.palette.secondary.main }} />}
+        subTitle={"Sales obtained"}
+        data={data2}
+        scheme={"category10"}
+        percentage={"+21%"}
+      />
+      <Card
+        title={"32,441"}
+        icon={<PersonAddIcon sx={{ fontSize: "23px", color: theme.palette.secondary.main }} />}
+        subTitle={"New Clients"}
+        data={data3}
+        scheme={"accent"}
+        percentage={"+5%"}
+      />
+      <Card
+        title={"1,325,134"}
+        icon={<TrafficIcon sx={{ fontSize: "23px", color: theme.palette.secondary.main }} />}
+        subTitle={"Traffic Received"}
+        data={data4}
+        scheme={"dark2"}
+        percentage={"+43%"}
+      />
+    </Stack>
+  );
 }
 
-export default Row1
+export default Row1;
--- src/page/dashboard/dashboard.jsx
+++ src/page/dashboard/dashboard.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import Row1 from './Row1'
-import Row2 from './Row2'
-import Row3 from './Row3'
-import { Button } from '@mui/material'
-import { DownloadOutlined } from '@mui/icons-material'
-
+import React from "react";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
+import { Box, Button } from "@mui/material";
+import { DownloadOutlined } from "@mui/icons-material";
 
 function dashboard() {
   return (
     <div>
-      <Button variant="text" color="primary">
-        <DownloadOutlined />
-        Download Reports
-      </Button>
+      <Box sx={{ textAlign: "right" }}>
+        <Button sx={{ padding: "6px 8px" }} variant="contained" color="primary">
+          <DownloadOutlined />
+          Download Reports
+        </Button>
+      </Box>
       <Row1 />
       <Row2 />
       <Row3 />
     </div>
-  )
+  );
 }
 
-export default dashboard
+export default dashboard;
